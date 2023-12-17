bd8073462cac8a3443a6bc7dbf2cdd54a8e436ff
Benjamin Kan <Kan@ibm.com>
2023-12-17 11:05:25 -0800

    Use AndroidX instead of support libraries

--- android/src/de/bsautermeister/snegg/services/GooglePlayGameServices.java
+++ android/src/de/bsautermeister/snegg/services/GooglePlayGameServices.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
+
+import androidx.annotation.NonNull;
 
 import com.badlogic.gdx.Gdx;
 import com.google.android.gms.games.Games;
